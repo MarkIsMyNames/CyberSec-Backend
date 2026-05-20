068134be18209700256a16159278c9a0736646ef
laptop <mrakemail@proton.me>
2026-05-20 20:28:21 +0100

    update method calls in user.py

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.logger import logger
@@ -18,14 +19,14 @@ class SQLUserRepository:
         logger.info("created user id=%d username=%s", user.id, username)
         return user
 
-    def get_user_by_username(self, username: str) -> type[User] | None:
-        user = self._session.query(User).filter_by(username=username).first()
+    def get_user_by_username(self, username: str) -> User | None:
+        user = self._session.scalar(select(User).where(User.username == username))
         if user is None:
             logger.debug("user not found username=%s", username)
         return user
 
-    def get_user_by_id(self, user_id: int) -> type[User] | None:
-        user = self._session.get(User, user_id)
+    def get_user_by_id(self, user_id: int) -> User | None:
+        user = self._session.scalar(select(User).where(User.id == user_id))
         if user is None:
             logger.debug("user not found id=%d", user_id)
         return user

99f79cfd1e28831747f4304fcab9644f08d20d90
laptop <mrakemail@proton.me>
2026-05-21 12:57:28 +0100

    simplify dependencies.py boilerplate

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,17 +1,12 @@
 from __future__ import annotations
 
-from contextlib import contextmanager
-
 from collections.abc import Generator
+from contextlib import contextmanager
 
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from app.session import _make_engine
-from app.repositories.group import SQLGroupRepository
-from app.repositories.key_bundle import SQLKeyBundleRepository
-from app.repositories.message import SQLMessageRepository
-from app.repositories.user import SQLUserRepository
 
 
 @contextmanager
@@ -25,19 +20,5 @@ def get_session() -> Generator[Session, None, None]:
         yield session
 
 
-def get_user_repo(session: Session = Depends(get_session)) -> SQLUserRepository:
-    return SQLUserRepository(session)
-
-
-def get_key_bundle_repo(
-    session: Session = Depends(get_session),
-) -> SQLKeyBundleRepository:
-    return SQLKeyBundleRepository(session)
-
-
-def get_message_repo(session: Session = Depends(get_session)) -> SQLMessageRepository:
-    return SQLMessageRepository(session)
-
-
-def get_group_repo(session: Session = Depends(get_session)) -> SQLGroupRepository:
-    return SQLGroupRepository(session)
+def repo_dep(cls):
+    return lambda session=Depends(get_session): cls(session)

7dd6a61d2fd8d58a985b0a5336433be59c481e53
laptop <mrakemail@proton.me>
2026-05-21 13:58:19 +0100

    don't need the cfg in ratelimit

--- app/auth/rate_limit.py
+++ app/auth/rate_limit.py
@@ -7,6 +7,5 @@ from app.config import config
 
 limiter = Limiter(key_func=get_remote_address)  # Limiter is per client
 
-_cfg = config["rate_limits"]
-AUTH_LIMIT: str = _cfg["auth"]
-MESSAGES_LIMIT: str = _cfg["messages"]
+AUTH_LIMIT: str = config["rate_limits"]["auth"]
+MESSAGES_LIMIT: str = config["rate_limits"]["messages"]
